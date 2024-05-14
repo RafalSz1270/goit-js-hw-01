541382f24cb5881abd3bafe37817000f20c4d538
Rafał Szymanek <rafal.szymanek.89@gmail.com>
2024-05-14 12:50:18 +0200

    corrections from the mentor

--- js/task-3.js
+++ js/task-3.js
@@ -3,12 +3,12 @@
                 b = padding.replace("px", ""),
                 c = border.replace("px", "");
 
-     console.log(a, b, c);
-     const elementWidth = Number(a) + Number(b) + Number(c);
-     console.log(elementWidth);
+     
+     const elementWidth = Number(a) + Number(b * 2) + Number(c * 2);
+     return(elementWidth);
     }
 
     
-console.log(getElementWidth("50px", "8px", "4px")); // 62
+console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
